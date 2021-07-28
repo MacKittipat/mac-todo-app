e7ec96648b5ac255f254b4140b4194fa4df27c49
Mac <mac.kittipat@gmail.com>
2021-07-28 13:04:14 +0700

    Init API Project

---
+++ mac-app-todo-api/src/main/java/com/mackittipat/macapptodoapi/MacAppTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macapptodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacAppTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacAppTodoApiApplication.class, args);
+	}
+
+}
---
+++ mac-app-todo-api/src/test/java/com/mackittipat/macapptodoapi/MacAppTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macapptodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacAppTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
