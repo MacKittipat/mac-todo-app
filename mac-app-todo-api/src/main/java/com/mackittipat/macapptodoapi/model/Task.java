63c0e1ebf2f41fb497e90c9ca48e8b5a31d6316d
Mac <mac.kittipat@gmail.com>
2021-07-28 15:34:14 +0700

    Add UUID field to task

--- mac-app-todo-api/src/main/java/com/mackittipat/macapptodoapi/model/Task.java
+++ mac-app-todo-api/src/main/java/com/mackittipat/macapptodoapi/model/Task.java
@@ -1,14 +1,20 @@
 package com.mackittipat.macapptodoapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 public class Task {
 
+  @JsonIgnore
   private Long id;
+
+  private String uuid = UUID.randomUUID().toString();
+
   private String message;
 
   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
